5befd190e6e389802f8216e350f33b5d9cc8488e
Alir3za <alireza2212kazemi@gmail.com>
2024-12-07 17:37:37 +0330

    template of page created

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-'use strict'
+"use strict";
+
+let days = document.querySelectorAll("button");
+
+for (let i = 0; i < days.length; i++) {
+	days[i].addEventListener("click", () => {
+    for (let j = 0; j < days.length; j++) {
+      days[j].classList.remove('button-back')
+    }
+		days[i].classList.add("button-back");
+	});
+}
+
+
