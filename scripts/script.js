4c4ec425e0082475540cbfb330d94f6af3af62c2
Alir3za <alireza2212kazemi@gmail.com>
2024-12-07 18:38:30 +0330

    some ui edits

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,16 @@
 "use strict";
 
-let days = document.querySelectorAll("button");
+let days = document.getElementsByTagName("button");
+let tables = document.getElementsByTagName('table');
 
 for (let i = 0; i < days.length; i++) {
 	days[i].addEventListener("click", () => {
     for (let j = 0; j < days.length; j++) {
-      days[j].classList.remove('button-back')
+      days[j].classList.remove('button-back');
+      tables[j].classList.add('hidden')
     }
 		days[i].classList.add("button-back");
+    tables[i].classList.remove('hidden')
 	});
 }
 
