05f03eaaaa8f3b577d890957052d0b0bb0769d26
Alir3za <alireza2212kazemi@gmail.com>
2024-12-08 16:05:39 +0330

    adding task and saving in localstorage

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let days = document.getElementsByClassName("weekdays-btn");
-let tables = document.getElementsByTagName("table");
+let tables = document.getElementsByTagName("TABLE");
 let addTaskBtn = document.getElementById("add-btn");
 let insertedTaskText = document.getElementById("task-textarea");
 let form = document.getElementById("form");
@@ -25,37 +25,29 @@ addTaskBtn.addEventListener("click", () => {
 	let date = Date.now();
 	objFormData.id = date.toString();
 	objFormData.isDone = objFormData.isDone ? true : false;
-	let stringData = JSON.stringify(objFormData);
-	LocalStorageValues.push(stringData);
+	LocalStorageValues.push(objFormData);
 	localStorage.setItem("tasks", JSON.stringify(LocalStorageValues));
-	createTask();
+	showTask();
 });
 
-function createTask() {
-	let parsedFormData = JSON.parse(localStorage.getItem("tasks"));
+let localData = [];
+let tableRow;
 
-	for (let i = 0; i < parsedFormData.length; i++) {
-		parsedFormData[i] = JSON.parse(parsedFormData[i]);
+function showTask() {
+	localData = JSON.parse(localStorage.getItem("tasks"));
+	for (let i = 0; i < tables.length; i++) {
+		for (const element of localData) {
+			if (tables[i].className.includes(`${element.dayInWeek}`)) {
+				tables[
+					i
+				].children[1].innerHTML = `<tr><th scope="row">1</th><td colspan="3" class="task-main">${
+					element.taskDescription
+				}</td><td colspan="1" class="check-cell"><input type="checkbox" name="done" id="" ${
+					element.isDone == true ? "checked" : null
+				}></td><td colspan="1" class="check-cell"><input type="checkbox" name="delete" id=""></td></tr>
+  `;
+			}
+		}
 	}
-
-	let desiredObj = parsedFormData.filter(
-		(param) => (param.id = parsedFormData[parsedFormData.length - 1].id)
-	);
-  
-	let targetTable = document.getElementsByClassName(
-		`${desiredObj[desiredObj.length - 1].dayInWeek}`
-	)[0];
-
-	let targetTableBody = targetTable.getElementsByTagName("tbody")[0];
-
-	targetTableBody.insertAdjacentHTML(
-		"beforeend",
-		`<tr><th scope="row">1</th><td colspan="3" class="task-main">${
-			desiredObj[desiredObj.length - 1].taskDescription
-		}</td><td colspan="1" class="check-cell"><input type="checkbox" name="done" id="" ${
-			desiredObj[desiredObj.length - 1].isDone == true ? "checked" : null
-		}></td><td colspan="1" class="check-cell"><input type="checkbox" name="delete" id=""></td></tr>
-  `
-	);
-	return;
 }
+showTask();
